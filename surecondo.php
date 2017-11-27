65303f7fa2426b681e15025b5060b137a1661b96
foreversaiz <foreversaiz@gmail.com>
2017-11-27 11:39:44 +0700

    Update surecondo.php

--- surecondo.php
+++ surecondo.php
@@ -46,7 +46,7 @@ if (!is_null($events['events'])) {
 
 
 			// Make a POST Request to Messaging API to reply to sender
-			$url = 'https://api.line.me/v2/bot/message/reply';
+			/*$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
 				'replyToken' => $replyToken,
 				'messages' => [$messages]
@@ -60,9 +60,9 @@ if (!is_null($events['events'])) {
 			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 			$result = curl_exec($ch);
-			//curl_close($ch);
+			curl_close($ch);*/
 
-
+			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data1 = [
 				'replyToken' => $replyToken,
 				'messages' => [$messages1]
