bfe405bb7a1a2cdd69b9df7b377b88ecc16d58b5
foreversaiz <foreversaiz@gmail.com>
2017-11-27 11:38:23 +0700

    Update surecondo.php

--- surecondo.php
+++ surecondo.php
@@ -60,7 +60,7 @@ if (!is_null($events['events'])) {
 			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 			$result = curl_exec($ch);
-			curl_close($ch);
+			//curl_close($ch);
 
 
 			$data1 = [
